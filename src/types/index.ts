772bcfa2d64806d3f7441c972b45cf245e7f952b
digression99 <digression99@gmail.com>
2023-01-13 21:00:29 +0900

    Update Block type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,12 @@
-export type BlockPosition = { x: number, y: number }
+export type Position = { x: number, y: number }
+
+export type FieldBitMap = number[][]
+export type BlockBitMap = number[][]
+
+export type Block = {
+  bitMap: BlockBitMap
+  isMerged: boolean
+  position: Position
+  color: 'red'
+  blockType: 'i' // types: i, o, j, l, t, s, z
+}
