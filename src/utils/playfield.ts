64546a8adb634a5d68b6869eb43c2cc3a4a8ac4b
digression99 <digression99@gmail.com>
2023-01-13 21:00:16 +0900

    Add mergeBlock

--- src/utils/playfield.ts
+++ src/utils/playfield.ts
@@ -1,43 +1,65 @@
-import { PLAYFIELD_HEIGHT, PLAYFIELD_PADDING, PLAYFIELD_WIDTH } from "../constants"
-import { BlockPosition } from "../types"
+import { PLAYFIELD_HEIGHT } from "../constants"
+import { BlockBitMap, Position, FieldBitMap } from "../types"
 
-export function drawBlock(block: number[][], field: number[][], pos: { x: number, y: number }) {
+export function drawBlock(block: number[][], field: number[][], pos: Position, prevPos: Position) {
   const { x, y } = pos
-
+  const { x: prevX, y: prevY } = prevPos
   const h = block.length
-  const l = block[0].length
+  const w = block[0].length
   const newField = field.map(row => row.slice())
 
   for (let i = 0; i < h; ++i) {
-    for (let j = 0; j < l; ++j) {
-      if (block[i][j] === 1)
+    for (let j = 0; j < w; ++j) {
+      if (block[i][j] === 1) {
+        newField[prevY + i][prevX + j] = 0
         newField[y + i][x + j] = 1
+      }
     }
   }
 
   return newField
 }
 
-export const calculatePosition = (pos: BlockPosition, key: string) => {
+export const calculatePosition = (pos: Position, key: string) => {
   if (key === 'w') {
-    return { ...pos, y: Math.max(0, pos.y - 1) }
-    // up
+    // TODO - rotation.
+    return pos
   } else if (key === 's') {
-    // - 2 : Shouldn't encounter the bottom.
-    return { ...pos, y: Math.min(PLAYFIELD_HEIGHT - 2, pos.y + 1) }
-    // down
+    return { ...pos, y: pos.y + 1 }
   } else if (key === 'a') {
-    return { ...pos, x: Math.max(PLAYFIELD_PADDING, pos.x - 1) }
-    // left
+    return { ...pos, x: pos.x - 1 }
   } else if (key === 'd') {
-    // add block width
-    const BLOCK_WIDTH = 4
-    return { ...pos, x: Math.min(PLAYFIELD_WIDTH + PLAYFIELD_PADDING - BLOCK_WIDTH, pos.x + 1) }
-    // right
+    return { ...pos, x: pos.x + 1 }
   } else if (key === ' ') {
-    // space
-    // hard drop
     return { ...pos, y: PLAYFIELD_HEIGHT - 1 }
   }
   return pos
+}
+
+export const detectCollision = (block: number[][], blockPosition: Position, field: number[][]) => {
+  const l = block[0].length
+  const h = block.length
+
+  for (let i = 0; i < h; ++i) {
+    for (let j = 0; j < l; ++j) {
+      if (block[i][j] === 1) {
+        const newPos = { x: blockPosition.x + j, y: blockPosition.y + i }
+        if (field[newPos.y][newPos.x] === 1) return true
+      }
+    }
+  }
+
+  return false
+}
+
+export function mergeBlock(field: FieldBitMap, block: BlockBitMap, blockPosition: Position) {
+  const l = block.length
+  const w = block[0].length
+  const newField = field.map(row => [...row])
+
+  for (let i = 0; i < l; ++i) {
+    for (let j = 0; j < w; ++j) { if (block[i][j] === 1) newField[blockPosition.y + i][blockPosition.x + j] = 1 }
+  }
+
+  return newField
 }
