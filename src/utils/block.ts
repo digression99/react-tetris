69a0ab38674b846f7375c13709a32ccf537e231b
digression99 <digression99@gmail.com>
2023-01-14 22:36:45 +0900

    Update block boundary check logic

--- src/utils/block.ts
+++ src/utils/block.ts
@@ -1,46 +1,31 @@
-import { PLAYFIELD_HEIGHT, PLAYFIELD_PADDING, PLAYFIELD_WIDTH } from "../constants"
-import { BlockBitMap, Position } from "../types"
+import { BlockBitMap, FieldBitMap, Position } from "../types"
 
-export function getBlockDimension(blockBitMap: BlockBitMap) {
-  const h = blockBitMap.length
-  const w = blockBitMap[0].length
+export function isBlockInBoundary(nextPos: Position, pos: Position, block: BlockBitMap, field: FieldBitMap) {
+  const l = block.length
+  const w = block[0].length
+  const { x, y } = pos
+  const { x: nextX, y: nextY } = nextPos
 
-  let minWidth = w - 1,
-    minHeight = h - 1,
-    maxWidth = 0,
-    maxHeight = 0
+  let isPixelInBoundary = true
 
-  for (let i = 0; i < h; ++i) {
+  const newField = field.map(row => [...row])
+
+  for (let i = 0; i < l; ++i) {
     for (let j = 0; j < w; ++j) {
-      // wow
-      if (blockBitMap[i][j] === 1) {
-        minWidth = Math.min(minWidth, j)
-        maxWidth = Math.max(maxWidth, j)
-        minHeight = Math.min(minHeight, i)
-        maxHeight = Math.min(maxHeight, i)
+      if (block[i][j] === 1) {
+        newField[i + y][j + x] = 0
       }
     }
   }
 
-  return {
-    width: maxWidth - minWidth + 1,
-    height: maxHeight - minHeight + 1
+  for (let i = 0; i < l; ++i) {
+    for (let j = 0; j < w; ++j) {
+      if (block[i][j] === 1 && newField[i + nextY][j + nextX] === 1) {
+        isPixelInBoundary = false
+      }
+    }
   }
+
+  return isPixelInBoundary
 }
 
-export function isBlockInBoundary(pos: Position, block: BlockBitMap) {
-  // should check the entire block pixel, not just the dimension.
-  const blockDimension = getBlockDimension(block)
-  // should check the block height.
-  const isYPosInBoundary = pos.y >= 0 && pos.y < PLAYFIELD_HEIGHT + blockDimension.height - 1
-  // should check the block width.
-  const isXPosInBoundary = pos.x >= PLAYFIELD_PADDING && pos.x < PLAYFIELD_WIDTH + PLAYFIELD_PADDING - blockDimension.width
-  return isYPosInBoundary && isXPosInBoundary
-}
-
-// This can't be implemented, since we need to know the intended position is 
-// already filled.
-export function isPixelInBoundary(pixelPosition: Position) {
-  // const isYPosInBoundary = //
-  return false
-}
