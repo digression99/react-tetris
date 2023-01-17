b8e707338a85de364d5bf7b5c2e29a3a702f6d76
digression99 <digression99@gmail.com>
2023-01-17 11:29:13 +0900

    Add BlockRotation type

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,6 +2,7 @@ export type Position = { x: number, y: number }
 
 export type BlockColor = 'red' | 'cyan' | 'orange' | 'blue' | 'red' | 'green' | 'purple' | 'yellow'
 export type BlockType = 'i' | 'o' | 'j' | 'l' | 't' | 's' | 'z'
+export type BlockRotation = 'clockwise' | 'c-clockwise'
 
 export type FieldBitMap = number[][]
 export type BlockBitMap = number[][]
