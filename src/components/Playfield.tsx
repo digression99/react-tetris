905989e72f6622453860cac04312aae257ab138f
digression99 <digression99@gmail.com>
2023-01-26 19:30:28 +0900

    Reduce time on counter

--- src/components/Playfield.tsx
+++ src/components/Playfield.tsx
@@ -1,40 +1,36 @@
-import { Box, Button } from '@chakra-ui/react'
+import { Heading, Box, Button } from '@chakra-ui/react'
 import { useBlock } from '../hooks/useBlock'
 import { useCountTime } from '../hooks/base/useCountTime'
 import { usePlayfield } from '../hooks/usePlayfield'
 import { useGameController } from '../hooks/useGameController'
-import { useEffect } from 'react'
+import { useEffectDebugger } from '../hooks/base/useEffectDebugger'
 
 export function Playfield() {
-  const { initializeField, changeGameStatus, gameStatus, fieldBuffer, field } = usePlayfield()
-  const { initializeBlock, currentBlock, changeBlockPosition } = useBlock()
+  const { reduceTime, timeCount, initializeField, changeGameStatus, gameStatus, fieldBuffer, field } = usePlayfield()
+  const { initializeBlock, currentBlock } = useBlock()
 
   useGameController()
 
   const { start, stop } = useCountTime((t: number) => {
     if (!field || !currentBlock) return
+    reduceTime(50)
+  }, 50, [fieldBuffer, currentBlock])
 
-    changeBlockPosition({
-      ...currentBlock.position,
-      y: currentBlock.position.y + 1
-    })
-  }, [fieldBuffer, currentBlock])
-
-  useEffect(() => {
+  useEffectDebugger(() => {
+    // NOTE - this effect is called numerously, 
+    // since deps include start, stop.
     if (gameStatus === 'init') {
       initializeField()
       initializeBlock()
       changeGameStatus('pending')
-      console.log('game is pending..')
     } else if (gameStatus === 'done') {
-      console.log('Game Over.')
       stop()
     } else if (gameStatus === 'started') {
-      console.log('start called.')
       start()
     }
     // TODO - show score page, start again page.
-  }, [start, stop, changeGameStatus, gameStatus, initializeField, initializeBlock])
+  }, [changeGameStatus, gameStatus, initializeBlock, initializeField, start, stop],
+    ['changeGameStatus', 'gameStatus', 'initializeBlock', 'initializeField', 'start', 'stop'])
 
   const onStart = () => {
     if (!currentBlock) return
@@ -47,6 +43,8 @@ export function Playfield() {
 
   return (
     <div>
+      <Heading>{timeCount}</Heading>
+
       {gameStatus === 'pending' && (
         <Button onClick={onStart}>Start!</Button>
       )}
